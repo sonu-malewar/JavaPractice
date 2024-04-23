b07488b311d0e54f0cfd337039880a3721b9ef61
S-Malewar <malewarsonu@gmail.com>
2024-04-23 13:51:08 +0530

    print array added

---
+++ src/JavaPractice/PrintArray.java
@@ -0,0 +1,14 @@
+package JavaPractice;
+
+import java.util.Arrays;
+
+public class PrintArray {
+	
+	public static void main(String[] args) {
+		
+		int arr[] = {4,5,5,6};
+		
+		System.out.println(Arrays.toString(arr));
+	}
+
+}
